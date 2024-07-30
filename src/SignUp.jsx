239a7b2f4d88ecb47ee38d5462466b6960369407
Molugu Deepak <molugudeepak@MacBook-Air.lan>
2024-07-30 15:37:14 -0400

    Initial commit

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -7,20 +7,36 @@ const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [backend, setBackend] = useState('python');
+  const [database, setDatabase] = useState('database');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    // console.log(backend)
+    if (backend == "python") {
     // Replace URL with your actual backend URL
-    const url = `http://localhost:5000/signup?backend=${backend}`;
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ username, email, password }),
-    });
-    const data = await response.json();
-    console.log(data);
+      const url = "http://localhost:5001/signup";
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({ 
+          "username": username, 
+          "email": email, 
+          "password": password,
+          "database": database
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+    } else {
+      console.log("different backend has been selected =>", backend)
+    }
+
+    // const url = 'http://localhost:5001/signup'
+    // const response = await fetch(url)
+    // console.log(response)
   };
 
   return (
@@ -57,6 +73,14 @@ const SignUp = () => {
           <option value="java">Java</option>
           <option value="golang">Golang</option>
         </select>
+        <select
+          className="input-field"
+          value={database}
+          onChange={(e) => setDatabase(e.target.value)}
+        >
+          <option value="SQL">SQL</option>
+          <option value="MongoDb">MongoDb</option>
+        </select>
         <button type="submit" className="button">Sign Up</button>
       </form>
     </div>
@@ -64,3 +88,4 @@ const SignUp = () => {
 };
 
 export default SignUp;
+
